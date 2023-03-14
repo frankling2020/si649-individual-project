78163173b155f7c291fa52f902a1172a594951cb
frankling2020 <3036431296@qq.com>
2023-03-14 01:31:26 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -196,7 +196,7 @@ with tab2:
     )
     with st.expander("How to use this visualization?"):
         st.write("Hi")
-        agree = st.sidebar.checkbox('Disable slider to select state')
+        agree = st.checkbox('Disable slider to select state')
         
     if agree:
         state_selector = st.selectbox('Select State', state_ids, format_func=lambda x: state_names[state_ids.index(x)])
